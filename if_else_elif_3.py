fd0235618dfbf225700d8d15c666148230b10a6a
zhenia.petrov01 <zhenia.petrov01@yandex.ru>
2024-11-20 18:18:04 +0300

    mew commit

--- if_else_elif_3.py
+++ if_else_elif_3.py
@@ -4,11 +4,11 @@ pie = 84
 money = int(input('Сколько у mеня с собой деньжат?'))
 if bun <= money < pie:
     print('Я могу себе позволить только булочку')
-if money >= pie and money < (bun * 2):
+elif money >= pie and money < (bun * 2):
     print('В этот раз куплю себе пирожок!!!')
-if money >= (bun * 2) and money < (pie * 2):
+elif money >= (bun * 2) and money < (pie * 2):
     print('Куплю ка я себе лучше 2 булочки')
-if money >= (pie * 2):
+elif money >= (pie * 2):
     print('Сегодня жирую) Хватает аж на 2 пирожка!!!')
 else:
     print('Может в следующий раз будут деньги...')
